54588ac7fd3fba58f13cc9a24dcaa35ced8958aa
cliffzxc <ericzkx@hotmail.com>
2015-07-14 17:15:56 +0800

    modify the id and bug

--- src/ios/CDVSharePlugin.m
+++ src/ios/CDVSharePlugin.m
@@ -21,14 +21,16 @@
     NSString* url = [[NSString alloc] initWithString:[command argumentAtIndex:2 withDefault:@"null"]];
     
 //    [[[UIAlertView alloc]initWithTitle:@" " message:imgSrc delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
-    [UMSocialWechatHandler setWXAppId:@"wxd930ea5d5a258f4f" appSecret:@"db426a9829e4b49a0dcac7b4162da6b6" url:url];
-    
+//    [UMSocialWechatHandler setWXAppId:@"wxd930ea5d5a258f4f" appSecret:@"db426a9829e4b49a0dcac7b4162da6b6" url:url];
+    [UMSocialData defaultData].extConfig.wechatSessionData.url = url;
+    [UMSocialData defaultData].extConfig.wechatTimelineData.url = url;
+
     [UMSocialSnsService presentSnsIconSheetView:[super viewController] appKey:nil shareText:text shareImage:[UIImage imageNamed:imgSrc] shareToSnsNames:[NSArray arrayWithObjects:UMShareToWechatSession,UMShareToWechatTimeline,UMShareToSina,nil] delegate:nil];
     
     [self.commandDelegate runInBackground:^{
-        
+
         CDVPluginResult* pluginResult = nil;
-        NSString* myarg = [command.arguments objectAtIndex:0];
+        NSString* myarg = [command argumentAtIndex:0];
         
         
         
